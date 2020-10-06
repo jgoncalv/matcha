811f5c5af7e3fe771fd5765aca0b849112825c90
jgoncalv <gon.jimmy@hotmail.fr>
2020-10-06 19:36:01 +0200

    Creation and use of authTokenDecoderMiddleware

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+
+exports.authTokenDecoderMiddleware = function(req, res, next) {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'Bearer'
+  ) {
+    token = req.headers.authorization.split(' ')[1]
+  } else if (req.query && req.query.token) {
+    token = req.query.token
+  } else if (
+    req.headers.cookie &&
+    req.headers.cookie.split('=')[0] === 'authToken'
+  ) {
+    token = req.headers.cookie.split('=')[1]
+  }
+
+  if (!token) return next()
+
+  jwt.verify(token, 'toto', (err, decoded) => {
+    if (err) return res.status(400).send()
+
+    req.user = decoded
+    next()
+  })
+}
+
+exports.isLoggedMiddleware = (req, res, next) => {
+  if (!req.user) return res.status(401).send()
+  next()
+}
