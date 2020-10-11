b618ef4b05d207eda83c2871e7e299c31328a89e
jgoncalv <gon.jimmy@hotmail.fr>
2020-10-11 16:53:05 +0200

    switching layout if user is connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Sign from './layouts/Sign';
+import Default from './layouts/Default';
 
 function App() {
+  const isConnected = useSelector(state => state.user.isConnected);
+
   return (
     <div>
-      <Sign />
+      {isConnected ? <Default/> : <Sign/>}
     </div>
   );
 }
